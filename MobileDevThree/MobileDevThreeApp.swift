796c8a001ffdc67830b5b659a9c3d564b87d710a
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-09-26 13:51:35 +0300

    Initial Commit

---
+++ MobileDevThree/MobileDevThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDevThreeApp.swift
+//  MobileDevThree
+//
+//  Created by Danil Peregorodiev on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MobileDevThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
